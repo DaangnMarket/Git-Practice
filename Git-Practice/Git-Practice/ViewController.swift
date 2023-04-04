7220cf760f31f1608cf911416c4d89f95689ee89
Mini <minnnidev@gmail.com>
2023-04-04 20:53:36 +0900

    [Feat] #1 - 민쓰 테스트

---
+++ Git-Practice/Git-Practice/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Git-Practice
+//
+//  Created by 김민 on 2023/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        minTest()
+    }
+    
+    // MARK: - Methods
+
+    private func minTest() {
+        view.backgroundColor = .yellow
+        print("Min")
+    }
+}
